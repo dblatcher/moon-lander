5468f543bf242b96010b2df6ce4021f3900d615a
David Blatcher <david.blatcher@graduate-jobs.com>
2021-10-21 08:22:16 +0100

    convert FullCanvas to function Component

--- components/FullCanvas.tsx
+++ components/FullCanvas.tsx
@@ -1,47 +1,19 @@
 import { ViewPort, World } from "physics-worlds";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
+export default function CanvasFull(props: {
+    world: World
+    magnify?: number
+}) {
+    const { world, magnify = 1 } = props
+    const canvas = useRef<HTMLCanvasElement | null>(null)
+    const viewPort = useRef<ViewPort | null>(null)
 
-export default class FullCanvas extends React.Component {
-    props!: {
-        world: World
-        magnify?: number
-    }
-    state!: {
-        viewPort?: ViewPort
-    }
-    canvasRef: React.RefObject<HTMLCanvasElement>;
-
-
-    constructor(props: FullCanvas["props"]) {
-        super(props);
-        this.state = {
-            viewPort: undefined
+    useEffect(() => {
+        if (canvas.current) {
+            viewPort.current = ViewPort.full(world, canvas.current, magnify)
         }
-        this.canvasRef = React.createRef();
+    })
 
-        this.createViewPort = this.createViewPort.bind(this);
-    }
-
-
-    createViewPort():void {
-        const { world, magnify = 1 } = this.props
-        const canvas = this.canvasRef.current
-
-        if (canvas) {
-            this.setState({
-                viewPort: ViewPort.full(world, canvas, magnify)
-            })
-        } else {
-            console.log('no canvas!')
-        }
-    }
-
-    componentDidMount() {
-        this.createViewPort()
-    }
-
-    render() {
-        return <canvas ref={this.canvasRef}></canvas>
-    }
+    return <canvas ref={canvas}></canvas>
 }
