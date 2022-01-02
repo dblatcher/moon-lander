1fb30dcf11236a283b70fae8a4888788641e62b6
dblatcher <dv.bltchr@gmail.com>
2022-01-02 01:58:23 +0000

    thrust noise stops when Spaceship leaves World

--- modules/world-things/SpaceShip.ts
+++ modules/world-things/SpaceShip.ts
@@ -92,12 +92,12 @@ class SpaceShip extends Body {
     }
 
     tick() {
-        const { shootCooldownCurrent = 0, thrust = 0, maxThrust=1, fuel: currentFuel = 1, maxFuel = 1 } = this.data;
+        const { shootCooldownCurrent = 0, thrust = 0, maxThrust = 1, fuel: currentFuel = 1, maxFuel = 1 } = this.data;
         const { landingPadIsRestingOn } = this
         if (shootCooldownCurrent > 0) { this.data.shootCooldownCurrent = shootCooldownCurrent - 1 }
 
         if (this.world.soundDeck && !this.thrustNoise) {
-            this.thrustNoise = this.world.soundDeck.playNoise({frequency:100},{loop:true, volume:0})
+            this.thrustNoise = this.world.soundDeck.playNoise({ frequency: 100, duration: 3 }, { loop: true, volume: 0 })
         }
         if (this.thrustNoise) {
             this.thrustNoise.volume = thrust / maxThrust
@@ -320,7 +320,7 @@ class SpaceShip extends Body {
     }
 
     leaveWorld(): void {
-        Body.prototype.leaveWorld.apply(this,[])
+        Body.prototype.leaveWorld.apply(this, [])
         this.thrustNoise?.stop()
     }
 
