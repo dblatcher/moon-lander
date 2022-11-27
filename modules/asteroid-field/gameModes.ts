5bfb63271d523b10a94514dd85adecaec469b1f2
dblatcher <dv.bltchr@gmail.com>
2022-11-27 13:03:29 +0000

    another level

--- modules/asteroid-field/gameModes.ts
+++ modules/asteroid-field/gameModes.ts
@@ -14,6 +14,7 @@ const gameModes: { [index: string]: GameMode } = {
         levelFunctions: [
             normal.level1,
             normal.level2,
+            normal.level3,
         ]
     }),
 }
--- modules/asteroid-field/level-factory/normal/level3.ts
+++ modules/asteroid-field/level-factory/normal/level3.ts
@@ -1,26 +1,25 @@
 import { World, StarField } from "physics-worlds";
 import { Level } from "../../../Level";
-import { murkyPlanet } from "../../../patterns/gradientFills";
 import { loadImageFill } from "../../../patterns/imageFills";
 import { DistantPlanet } from "../../../world-things/DistantPlanet";
 import { generateRocks, makeShip } from "../thingFactories";
 
 
-export async function level2(): Promise<Level> {
+export async function level3(): Promise<Level> {
 
     const [jupiter, neptune] = await Promise.all([
-        loadImageFill('jupiter', 'red', { parallax: 2 }),
-        loadImageFill('neptune', 'blue', { parallax: 2, scale: 3 }),
+        loadImageFill('jupiter', 'red', { parallax: 2, scale: 4 }),
+        loadImageFill('neptune', 'blue', { parallax: 3 }),
     ])
 
     const worldDimensions = {
-        width: 1200,
+        width: 1600,
         height: 1200,
     }
 
     const world = new World([
         makeShip(worldDimensions.width / 2, worldDimensions.height / 2),
-        ...generateRocks(2, worldDimensions, {
+        ...generateRocks(4, worldDimensions, {
             sizes: [70, 100],
             avoid: { x: worldDimensions.width / 2, y: worldDimensions.height / 2, radius: 200 },
             speedRange: [0, 8]
@@ -33,17 +32,17 @@ export async function level2(): Promise<Level> {
             new StarField({ numberOfStars: 50, depth: 4, ...worldDimensions }),
             new StarField({ numberOfStars: 25, depth: 8, ...worldDimensions }),
             new DistantPlanet({
-                x: 1000,
-                y: 250,
-                radius: 400,
-                parallax: 4,
-                fillColor: murkyPlanet,
+                x: 700,
+                y: 200,
+                radius: 640,
+                parallax: 2,
+                fillColor: jupiter,
             }),
             new DistantPlanet({
-                x: 400,
-                y: 630,
-                radius: 480,
-                parallax: 2,
+                x: 1100,
+                y: 230,
+                radius: 320,
+                parallax: 3,
                 fillColor: neptune,
             }),
         ],
