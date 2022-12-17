51601647166a7e8bbc05dc79e82896ae2ebfb105
dblatcher <dv.bltchr@gmail.com>
2022-12-17 21:53:04 +0000

    do the game modes for asteroids right

--- modules/asteroid-field/gameModes.ts
+++ modules/asteroid-field/gameModes.ts
@@ -2,22 +2,33 @@ import * as normal from "./level-factory/normal";
 import { GameMode } from "../GameMode";
 
 
+const modeConfig = {
+    speed: 40,
+    noScores: false,
+    levelFunctions: [
+        normal.level1,
+        () => { return normal.makeProceduralLevel(1) },
+        normal.level2,
+        normal.level3,
+    ],
+    makeProceduralLevel: normal.makeProceduralLevel
+}
+
 const gameModes: { [index: string]: GameMode } = {
     'normal': new GameMode({
         title: "arcade",
         key: "normal",
+        ...modeConfig
+    }),
+    'trainer': new GameMode({
+        key: 'trainer',
+        title: 'trainer',
+        ...modeConfig,
         startingLives: Infinity,
-        speed: 40,
-        noScores: false,
         allowSkip: true,
         allowRestart: true,
-        levelFunctions: [
-            normal.level1,
-            () => { return normal.makeProceduralLevel(1) },
-            normal.level2,
-            normal.level3,
-        ],
-        makeProceduralLevel: normal.makeProceduralLevel
+        noScores: true,
+        hidden: true,
     }),
 }
 
