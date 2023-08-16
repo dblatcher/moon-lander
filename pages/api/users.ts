eebdcd721dfe94e956d9eaca7f1679e4a7f9a582
dblatcher <dv.bltchr@gmail.com>
2023-08-16 23:19:47 +0100

    get user function with seed table fall back

---
+++ pages/api/users.ts
@@ -0,0 +1,87 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { sql, QueryResult, QueryResultRow } from '@vercel/postgres';
+import { seed } from '../../lib/seed';
+
+type Data = {
+    users?: QueryResult<QueryResultRow>
+    error?: string
+}
+
+
+type PostgresException = {
+    name: string;
+    code: string;
+    message: string;
+}
+
+const ERROR_CODES = {
+    undefined_table: "42P01"
+}
+
+const parseError = (error: unknown): PostgresException | undefined => {
+    if (!error || typeof error !== 'object') {
+        return undefined
+    }
+    const record = error as Record<string, unknown>;
+
+    if (typeof record['code'] !== 'string') {
+        return
+    }
+
+    return record as PostgresException
+}
+
+const getAll = async (res: NextApiResponse<Data>) => {
+    try {
+        const users = await sql`SELECT * FROM Users;`;
+        return res.status(200).json({ users })
+    } catch (error) {
+        const postgresException = parseError(error)
+
+        if (postgresException?.code === ERROR_CODES.undefined_table) {
+            console.log(
+                'Table does not exist, creating and seeding it with dummy data now...'
+            )
+            await seed()
+            const users = await sql`SELECT * FROM Users;`;
+            return res.status(200).json({ users })
+        }
+
+        return res.status(400).json({ error: 'select fail' })
+    }
+}
+
+const handlePost = async (
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) => {
+
+    console.log(req.body)
+
+    const { email, name, image } = req.body
+
+    if (typeof email !== 'string' || typeof name !== 'string' || typeof image !== 'string') {
+        return res.status(400).json({ error: 'invalid input' })
+    }
+
+    try {
+        await sql`INSERT INTO Users (Email, Name, Image) VALUES (${email}, ${name}, ${image});`;
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: 'insert fail' })
+    }
+
+    return getAll(res)
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    if (req.method === 'POST') {
+        return handlePost(req, res)
+    }
+
+    return getAll(res)
+}
